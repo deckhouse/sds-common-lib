9b9c1b3dd73a8040def987fee312446649489af8
v.oleynikov <vasily.oleynikov@flant.com>
2025-03-22 07:51:21 +0300

    Add common client
    Signed-off-by: v.oleynikov <vasily.oleynikov@flant.com>

---
+++ kubernets.go
@@ -0,0 +1,32 @@
+package handlers
+
+import (
+	apiruntime "k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+func NewKubeClient(kubeconfigPath string, schemeFuncs ...func(*apiruntime.Scheme) error) (client.Client, error) {
+	var config *rest.Config
+	var err error
+
+	config, err = clientcmd.BuildConfigFromFlags("", kubeconfigPath)
+	if err != nil {
+		return nil, err
+	}
+
+	scheme := apiruntime.NewScheme()
+
+	for _, f := range schemeFuncs {
+		if err = f(scheme); err != nil {
+			return nil, err
+		}
+	}
+
+	clientOpts := client.Options{
+		Scheme: scheme,
+	}
+
+	return client.New(config, clientOpts)
+}
