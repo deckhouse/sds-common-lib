0c2938b30fa75049b59db8aead1f00d2144f776a
Aleksandr Stefurishin <aleksandr.stefurishin@flant.com>
2025-04-28 12:04:14 +0300

    remove unused argument (#6)
    Signed-off-by: Aleksandr Stefurishin <aleksandr.stefurishin@flant.com>

--- kubeclient/kubeclient.go
+++ kubeclient/kubeclient.go
@@ -17,22 +17,24 @@ limitations under the License.
 package kubeclient
 
 import (
+	"fmt"
+
 	apiruntime "k8s.io/apimachinery/pkg/runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
 )
 
-func New(kubeconfigPath string, schemeFuncs ...func(*apiruntime.Scheme) error) (client.Client, error) {
+func New(schemeFuncs ...func(*apiruntime.Scheme) error) (client.Client, error) {
 	kubeConfig, err := config.GetConfig()
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("getting config: %w", err)
 	}
 
 	scheme := apiruntime.NewScheme()
 
 	for _, f := range schemeFuncs {
 		if err = f(scheme); err != nil {
-			return nil, err
+			return nil, fmt.Errorf("building scheme: %w", err)
 		}
 	}
 
