8f52e7953bfa8e7009511ca87dff9eeddb8474d6
Aleksandr Stefurishin <aleksandr.stefurishin@flant.com>
2025-06-10 17:41:26 +0300

    Exponential cooldown (#7)
    Signed-off-by: Aleksandr Stefurishin <aleksandr.stefurishin@flant.com>

---
+++ cooldown/exponential_cooldown.go
@@ -0,0 +1,74 @@
+package cooldown
+
+import (
+	"context"
+	"sync"
+	"time"
+)
+
+type ExponentialCooldown struct {
+	initialDelay time.Duration
+	maxDelay     time.Duration
+	mu           *sync.Mutex
+
+	// mutable:
+
+	lastHit           time.Time
+	nextCooldownDelay time.Duration
+}
+
+func NewExponentialCooldown(
+	initialDelay time.Duration,
+	maxDelay time.Duration,
+) *ExponentialCooldown {
+	if initialDelay < time.Nanosecond {
+		panic("expected initialDelay to be positive")
+	}
+	if maxDelay < initialDelay {
+		panic("expected maxDelay to be greater or equal to initialDelay")
+	}
+
+	return &ExponentialCooldown{
+		initialDelay:      initialDelay,
+		maxDelay:          maxDelay,
+		mu:                &sync.Mutex{},
+		nextCooldownDelay: initialDelay,
+	}
+}
+
+// If not in cooldown, returns immediately, and starts the cooldown.
+// If in cooldown, waits until cooled and returns nil. Next delay will be
+// doubled, but always shorter then maxDelay and longer then initialDelay.
+func (cd *ExponentialCooldown) Hit(ctx context.Context) error {
+	if err := ctx.Err(); err != nil {
+		return err
+	}
+
+	cd.mu.Lock()
+	defer cd.mu.Unlock()
+
+	// repeating cancelation check, since lock may have taken a long time
+	if err := ctx.Err(); err != nil {
+		return err
+	}
+
+	sinceLastHit := time.Since(cd.lastHit)
+
+	if sinceLastHit >= cd.nextCooldownDelay {
+		// cooldown has passed by itself - resetting the delay
+		cd.lastHit = time.Now()
+		cd.nextCooldownDelay = cd.initialDelay
+		return nil
+	}
+
+	// inside a cooldown
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	case <-time.After(cd.nextCooldownDelay - sinceLastHit):
+		// cooldown has passed just now - doubling the delay
+		cd.lastHit = time.Now()
+		cd.nextCooldownDelay = min(cd.nextCooldownDelay*2, cd.maxDelay)
+		return nil
+	}
+}
---
+++ cooldown/exponential_cooldown_test.go
@@ -0,0 +1,85 @@
+package cooldown
+
+import (
+	"context"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestExponentialCooldown_Hit_DelayGrows(t *testing.T) {
+	cd := NewExponentialCooldown(100*time.Millisecond, time.Second)
+
+	var lastDelay time.Duration
+	for range 5 {
+		start := time.Now()
+		if err := cd.Hit(context.Background()); err != nil {
+			t.Errorf("expected nil err, got %v", err)
+		}
+		delay := time.Since(start)
+		if delay <= lastDelay {
+			t.Errorf(
+				"expected delay to grow after each hit, got %v->%v",
+				lastDelay, delay,
+			)
+		}
+		lastDelay = delay
+	}
+}
+
+func TestExponentialCooldown_Hit_ContextCanceled(t *testing.T) {
+	// arrange
+	dop := 10
+	delay := time.Second
+	beforeCancelDelay := 100 * time.Millisecond
+	firstCallTimeout := 5 * time.Millisecond
+	ctx, cancel := context.WithCancel(context.Background())
+
+	// act
+	cd := NewExponentialCooldown(delay, delay)
+	firstCallCtx, firstCallCancel := context.WithTimeout(ctx, firstCallTimeout)
+	defer firstCallCancel()
+	firstErr := cd.Hit(firstCallCtx)
+
+	var errs []error
+	errsMu := &sync.Mutex{}
+
+	wg := &sync.WaitGroup{}
+	wg.Add(dop)
+
+	for range dop {
+		go func() {
+			err := cd.Hit(ctx)
+			errsMu.Lock()
+			errs = append(errs, err)
+			errsMu.Unlock()
+			wg.Done()
+		}()
+	}
+
+	// to avoid early cancelation
+	time.Sleep(beforeCancelDelay)
+
+	cancel()
+	wg.Wait()
+
+	// assert
+	if firstErr != nil {
+		t.Errorf("expected first Hit to return nil, got %v''", firstErr)
+
+	}
+	if len(errs) != dop {
+		t.Errorf("expected %d errors, got %d", dop, len(errs))
+	}
+
+	for _, err := range errs {
+		// strict check
+		if err != context.Canceled {
+			t.Fatalf(
+				"expected all errors to be '%v', got '%v'",
+				context.Canceled,
+				err,
+			)
+		}
+	}
+}
